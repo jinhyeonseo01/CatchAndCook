b5d47f228acf198576fc7cd24ea59e64e7c6a9ea
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-07-02 15:30:30 +0900

    버그픽스

--- CatchAndCook/ResourceManager.cpp
+++ CatchAndCook/ResourceManager.cpp
@@ -360,7 +360,7 @@ void ResourceManager::CreateDefaultShaderJIN()
 		info._zTest = true;
 		info._zWrite = false;
 		info._stencilTest = false;
-		info.cullingType = CullingType::BACK;
+		info.cullingType = CullingType::FRONT;
 
 		shared_ptr<Shader> shader = ResourceManager::main->Load<Shader>(L"Skybox", L"Skybox_Field.hlsl", GeoMetryProp,
 			ShaderArg{}, info);
